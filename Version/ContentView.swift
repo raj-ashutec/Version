cc3ce47ac735a74c5c3ca25c168e3b0dfd6d62ac
Raj <rjbhatt110@gmail.com>
2021-06-09 16:46:47 +0530

    Initial Commit

---
+++ Version/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Version
+//
+//  Created by Ashutec Apple on 09/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Version/VersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionApp.swift
+//  Version
+//
+//  Created by Ashutec Apple on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct VersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
